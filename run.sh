d7872226a4fe37823628ba6fb9cca2c9d65c0a7c
Michael Young <youngmd@indiana.edu>
2016-06-13 15:06:06 -0400

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#make sure jq is installed on $SCA_SERVICE_DIR
+if [ ! -f $SCA_SERVICE_DIR/jq ];
+then
+        echo "installing jq"
+        wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O $SCA_SERVICE_DIR/jq
+        chmod +x $SCA_SERVICE_DIR/jq
+fi
+
+buildid=`jq -r '.[build_task_id]' config.json`
+docker -d -P $buildid /usr/sbin/sshd -D
