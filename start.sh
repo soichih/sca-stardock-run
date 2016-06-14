190e4e89a1df6983563489ebeef04975a6fb7c42
Michael Young <youngmd@indiana.edu>
2016-06-14 15:09:13 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,3 +14,4 @@ buildid=`$SCA_SERVICE_DIR/jq -r '.build_task_id' config.json`
 imageid=`$SCA_SERVICE_DIR/jq -r .[0].imageid ../$buildid/products.json`
 docker run -d -P --name=$UUID $imageid /usr/sbin/sshd -D
 echo $UUID > container.name
+echo `docker port $UUID` > container.port
