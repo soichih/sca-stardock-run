405edd30a8dc9aac9a87c62091b699e244c92eb7
Michael Young <youngmd@indiana.edu>
2016-06-21 13:04:50 -0400

    Update status.sh

--- status.sh
+++ status.sh
@@ -4,5 +4,5 @@ status=docker inspect --type=container -f {{.State.Running}} $container
 
 if $status;
 then
-  curl -X POST -H "Content-Type: application/json" -d "{\"msg\":\"Container started"}" $SCA_PROGRESS_URL
+  curl -X POST -H "Content-Type: application/json" -d "{\"msg\":\"Container started\"}" $SCA_PROGRESS_URL
 fi
