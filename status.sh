a7e4edf15b4e1aee05007d183e9298b782623f20
Michael Young <youngmd@indiana.edu>
2016-06-21 13:08:00 -0400

    Update status.sh

--- status.sh
+++ status.sh
@@ -2,7 +2,9 @@ container=`cat container.name`
 
 status=docker inspect --type=container -f {{.State.Running}} $container
 
+port=`awk '{split($0,a,":"); print a[2]}' container.port`
+
 if $status;
 then
-  curl -X POST -H "Content-Type: application/json" -d "{\"msg\":\"Container started\"}" $SCA_PROGRESS_URL
+  curl -X POST -H "Content-Type: application/json" -d "{\"msg\":\"Container started on port $port\"}" $SCA_PROGRESS_URL
 fi
