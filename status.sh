f3349016d277748f269de23ef3aa4645397dcf47
Michael Young <youngmd@indiana.edu>
2016-07-12 12:03:47 -0400

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,6 +1,6 @@
 container=`cat container.name`
 cid=`docker ps -aqf "name=$container"`
-status=docker inspect --type=container -f {{.State.Running}} $container
+status=`docker inspect --type=container -f {{.State.Running}} $container`
 
 port=`awk '{split($0,a,":"); print a[2]}' container.port`
 host=`hostname`
